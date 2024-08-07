45f062332b63a60aeccd55075d3d870fdb4dce04
Darae0415 <ekfogksmf@naver.com>
2024-08-07 17:22:54 +0900

    0807 최종

--- src/ch01/sec06/Hello.java
+++ src/ch01/sec06/Hello.java
@@ -3,7 +3,11 @@ package ch01.sec06;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("I can push to github!!!");
+		int x = 1;
+		int y = 2;
+		int result = x+y;
+		System.out.println(result);
+		System.out.println("I can push \n to github!!!");
 
 	}
 
---
+++ src/ch01/sec11/Calculator.java
@@ -0,0 +1,13 @@
+package ch01.sec11;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		byte x=126;
+		int y=1;
+		int result= (byte)(x+y);
+		System.out.println(result);
+
+	}
+
+}
